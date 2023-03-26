393887febeaa5b965d17e0175848e1e44fa77e4a
romain <romain.dusson@gmail.com>
2023-03-26 23:52:05 +0200

    change buffer switch mapping

--- mappings.lua
+++ mappings.lua
@@ -15,6 +15,10 @@ return {
       end,
       desc = "Pick to close",
     },
+    ["<leader>bl"] = { 
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
+    ["<leader>bh"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous buffer",},
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
