f394a7eaa20f3feaea259b0e494a146a7076550e
romain <romain.dusson@gmail.com>
2023-03-28 22:24:47 +0200

    add new colorsheme

--- init.lua
+++ init.lua
@@ -18,7 +18,9 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+	colorscheme = "tokyonight-night",
+
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,13 @@
 return {
+    
+    "catppuccin/nvim", 
+    name = "catppuccin",
+    config = function()
+        require("catppuccin").setup {flavour = "mocha",}
+      end,
+    { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = true, priority = 1000 },
+    { "folke/tokyonight.nvim", name = "tokyonight" },
+
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
